c0c04e2b084e4d5c78742b5e741a77a1a456262e
danielsotoortiz <danielsotoortiz@localhost>
2024-01-03 23:00:11 +0100

    First version

---
+++ src/ut5/UT5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ut5;
+
+/**
+ *
+ * @author danielsotoortiz
+ */
+public class UT5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
