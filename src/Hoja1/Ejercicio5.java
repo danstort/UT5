592be481fc8cfebd3f2712b5bcbd983df1b4e070
daniel soto <daniel.soto.ortiz@gmail.com>
2024-01-04 18:39:53 +0100

    first step

---
+++ src/Hoja1/Ejercicio5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hoja1;
+
+/**
+ *
+ * @author danie
+ */
+public class Ejercicio5 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //Esto es una prueba
+        
+        System.out.println("Hola");
+    }
+}
