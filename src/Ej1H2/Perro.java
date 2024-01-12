04ad03b80869709f5e25b6ff7cfbffb1132211c3
danielsotoortiz <danielsotoortiz@macbook-air.alumnado.educarm.net>
2024-01-12 12:46:06 +0100

    First version

---
+++ src/Ej1H2/Perro.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ej1H2;
+
+/**
+ *
+ * @author danielsotoortiz
+ */
+public class Perro {
+
+    char color;
+    int edad;
+
+    public Perro() {
+    }
+
+    public void setColor(char color) {
+        this.color = color;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+}
